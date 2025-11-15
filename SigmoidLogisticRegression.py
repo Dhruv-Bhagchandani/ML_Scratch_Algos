cd3d0256760e074f06217fd2746448138aa0f28a
Dhruv-Bhagchandani <dv.dev1906@gmail.com>
2025-11-15 11:28:23 +0530

    Implementation of Logistic Regression via Sigmoid Function
    Same logic as Perceptron Trick Algorithm but we have used Sigmoid function instead of step function.
    Sigmoid Function improvements over Step Function : 
    1. Adds a probabalistic view to our classification
    2. Defines the rate at which to pull or push our logistic regression line as per our dataset

---
+++ SigmoidLogisticRegression.py
@@ -0,0 +1,31 @@
+import numpy as np 
+
+class PerceptronLogisticRegression:
+    def perceptron (X,y) : 
+        # Function parameters : X : input features, y : output features
+    
+        # Insert 1s in the first column of your input features for transformation purposes
+        np.insert(X,0,1,axis=1)
+
+        # Initialize weights(coefficients) to any desired values : we'll take all ones 
+        weights = np.ones(X.shape[1])
+
+        # Define learning rate : 
+        lr = 0.01  
+
+        # loop to reach to our desired logistic regression line : 
+        for i in (1000) : 
+            j = np.random.randint(0,100) # here 100 is number of datapoints in the dataset 
+
+            # Our model prediction : 
+            y_hat = sigmoid(np.dot(X[j],weights))
+        
+            # weights updation : 
+            weights = weights + lr*( y - y_hat )*X[j]
+
+        return weights[0],weights[1:]  # constant,coefficients
+
+    # Sigmoid function : 
+    def sigmoid(z):
+        return 1/( 1 + np.exp(-z) )
+
