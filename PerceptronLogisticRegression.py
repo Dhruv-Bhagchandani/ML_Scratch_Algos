2d41a89ad658e9fd6fd1a4c3e64ec1812ba734f1
Dhruv-Bhagchandani <dv.dev1906@gmail.com>
2025-11-15 09:08:19 +0530

    Implementation of Logistic Regression via Perceptron Trick
    Perceptron Trick is a mechanism via which we start with a random hyperplane ( by taking taking desired coefficients for our features ) in space and get to the best fit hyperplane which segregates categories of output features in the best possible way it can. To do this, we continuously take datapoints from our dataset, analyze it and then apply the necessary transformation that is required to make our line better with respect to that point.

---
+++ PerceptronLogisticRegression.py
@@ -0,0 +1,30 @@
+import numpy as np 
+
+class PerceptronLogisticRegression:
+    def perceptron (X,y) : 
+        # Function parameters : X : input features, y : output features
+    
+        # Insert 1s in the first column of your input features for transformation purposes
+        np.insert(X,0,1,axis=1)
+
+        # Initialize weights(coefficients) to any desired values : we'll take all ones 
+        weights = np.ones(X.shape[1])
+
+        # Define learning rate : 
+        lr = 0.01  
+
+        # loop to reach to our desired logistic regression line : 
+        for i in (1000) : 
+            j = np.random.randint(0,100) # here 100 is number of datapoints in the dataset 
+
+            # Our model prediction : 
+            y_hat = step(np.dot(X[j],weights))
+        
+            # weights updation : 
+            weights = weights + lr*( y - y_hat )*X[j]
+
+        return weights[0],weights[1:]  # constant,coefficients
+
+    def step(z):
+        return 1 if z > 0 else 0
+
